b711433120d65e5a9909a0f8eebf4596a38bb730
bindushree7259 <bindushreee806@gmail.com>
2026-08-10 11:18:39 +0530

    math module commit

---
+++ math.py
@@ -0,0 +1,10 @@
+import math
+
+num = 25
+
+print("Square root:", math.sqrt(num))
+print("Power:", math.pow(2, 3))
+print("Ceiling:", math.ceil(4.2))
+print("Floor:", math.floor(4.8))
+print("Factorial:", math.factorial(5))
+print("Pi value:", math.pi)
